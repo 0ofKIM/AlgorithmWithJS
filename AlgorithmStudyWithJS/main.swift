1343b3c8638d70ea1460d4f55b2923df9ee7fa00
uykim3 <uykim3@lotte.net>
2023-02-16 23:47:31 +0900

    first commit

---
+++ AlgorithmStudyWithJS/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  AlgorithmStudyWithJS
+//
+//  Created by 0ofKim on 2023/02/16.
+//
+
+import Foundation
+
+func solution() {
+    guard let number = readLine() else { return }
+    print(number)
+}
+
+solution()
