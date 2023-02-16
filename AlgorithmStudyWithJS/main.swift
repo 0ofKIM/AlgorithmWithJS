41400f7f48a85f6970b90f0e2363e578c5ef14cf
uykim3 <uykim3@lotte.net>
2023-02-17 00:00:25 +0900

    개수 세기(10807)

--- AlgorithmStudyWithJS/main.swift
+++ AlgorithmStudyWithJS/main.swift
@@ -5,11 +5,16 @@
 //  Created by 0ofKim on 2023/02/16.
 //
 
+// 개수 세기(10807)
 import Foundation
 
 func solution() {
-    guard let number = readLine() else { return }
-    print(number)
+    let _ = readLine()! // number count
+    let numberArray = readLine()!.components(separatedBy: " ").map({String($0)})
+    let findNumber = readLine()!
+
+    let result = numberArray.filter { $0 == findNumber }
+    print(result.count)
 }
 
 solution()
