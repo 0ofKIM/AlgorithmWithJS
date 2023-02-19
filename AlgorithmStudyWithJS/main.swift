0458d4666e92eef879a3d26864d8582a9052ac96
uykim3 <uykim3@lotte.net>
2023-02-19 23:56:54 +0900

    백준 - 최댓값(2562)

--- AlgorithmStudyWithJS/main.swift
+++ AlgorithmStudyWithJS/main.swift
@@ -5,14 +5,32 @@
 //  Created by 0ofKim on 2023/02/16.
 //
 
-// 최소, 최대(10818)
+// 최댓값 (2562)
 import Foundation
 func solution() {
-    let numberCount = Int(readLine()!)!
-    let numberArray = readLine()!.split(separator: " ").map { Int($0)! }
+    var maxNumber = 0
+    var maxNumberIndex = 0
+    for index in 1...9 {
+        let number = Int(readLine()!)!
+        if number > maxNumber {
+            maxNumber = number
+            maxNumberIndex = index
+        }
+    }
+
+    print(maxNumber)
+    print(maxNumberIndex)
+}
+solution()
+
+// 최소, 최대(10818)
+//import Foundation
+//func solution() {
+//    let numberCount = Int(readLine()!)!
+//    let numberArray = readLine()!.split(separator: " ").map { Int($0)! }
 
     //방법1. 460ms
-    print("\(numberArray.min()!) \(numberArray.max()!)")
+//    print("\(numberArray.min()!) \(numberArray.max()!)")
 
     //방법2. 440ms
 //    guard numberCount != 1 else {
@@ -27,8 +45,8 @@ func solution() {
 //        if array > maxNumber { maxNumber = array }
 //    }
 //    print("\(minNumber) \(maxNumber)")
-}
-solution()
+//}
+//solution()
 //import Foundation
 //func solution() {
 //    let _ = Int(readLine()!)!
